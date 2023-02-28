a00cffe0a3bad37d096c6fe5dd67a36c06beaa18
vf98 <vf98@AR5-Asistent.ftn.ns>
2023-02-28 15:09:14 +0100

    Prve vezbe 28.02.2023.

---
+++ RvaBackEndG5/src/main/java/rva/RvaBackEndG5Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvaBackEndG5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvaBackEndG5Application.class, args);
+	}
+
+}
