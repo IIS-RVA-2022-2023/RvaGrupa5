8717602693ca54bef8a76b76e922c3e722ff250a
VladimirFabri98 <vfficial98@gmail.com>
2023-04-20 18:12:14 +0200

    Osme vezbe 20.04.

---
+++ FrontEndG5/src/app/components/main/artikl/artikl.component.ts
@@ -0,0 +1,27 @@
+import { ArtiklService } from './../../../services/artikl.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artikl',
+  templateUrl: './artikl.component.html',
+  styleUrls: ['./artikl.component.css']
+})
+export class ArtiklComponent implements OnInit {
+  
+  constructor(private artiklService: ArtiklService){
+
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  public loadData(){
+    this.artiklService.getAllArtikls().subscribe(
+      data => {console.log(data);} 
+    ),
+    (error : Error) => {console.log(error.name + ' ' + error.message);}
+  }
+
+
+}
